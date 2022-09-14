2f9c08bfed8e441607173126b5807606d07b9868
abiolahenrys <abiolahenrys@gmail.com>
2022-09-13 17:09:09 -0700

    J

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,47 +1,46 @@
 #include "main.h"
 /**
- * print_times_table - times table function
- * @n: integer to return
+ * print_times_table - Prints a multiplication table up to param
+ * @n: The number to be treated
  *
+ * Return: Number matrix
  */
 void print_times_table(int n)
 {
-int i;
-int j;
-if (n <= 15 && n >= 0)
+int x, y, z;
+if (n >= 0 && n <= 14)
 {
-for (i = 0; i <= n; i++)
+for (x = 0; x <= n; x++)
 {
-for (j = 0; j <= n; j++)
+for (y = 0; y <= n; y++)
 {
-int prod = j * i;
-if (j == 0)
-{
-_putchar('0');
-}
-else if (prod <= 9)
+z = x * y;
+if (z > 99)
 {
 _putchar(',');
-_putchar(' ');
-_putchar(' ');
-_putchar(' ');
-_putchar('0' + prod);
+_putchar(32);
+_putchar((z / 100) + '0');
+_putchar(((z / 10) % 10) + '0');
+_putchar((z % 10) + '0');
 }
-else if (prod > 99)
+else if (z > 9)
 {
 _putchar(',');
-_putchar(' ');
-_putchar('0' + (prod / 100));
-_putchar('0' + ((prod / 10) % 10));
-_putchar('0' + (prod % 10));
+_putchar(32);
+_putchar(32);
+_putchar(((z / 10) % 10) + '0');
+_putchar((z % 10) + '0');
 }
 else
 {
+if (y != 0)
+{
 _putchar(',');
-_putchar(' ');
-_putchar(' ');
-_putchar('0' + (prod / 10));
-_putchar('0' + (prod % 10));
+_putchar(32);
+_putchar(32);
+_putchar(32);
+}
+_putchar(z + '0');
 }
 }
 _putchar('\n');
