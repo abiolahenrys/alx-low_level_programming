14b9e6e9a0c66462bf4c57542c11667088210672
Mac <mac@Mars-MacBook-Pro.local>
2022-10-27 13:21:44 +0100

    M

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _atoi - converts chars to integer
  *
@@ -21,13 +20,11 @@ unsigned int _atoi(char c)
 */
 unsigned int _strlen(const char *str)
 {
-	unsigned int index = 0;
-
-	while (str[index] != '\0')
-		index++;
-	return (index);
+unsigned int index = 0;
+while (str[index] != '\0')
+index++;
+return (index);
 }
-
 /**
  * binary_to_uint - a function that converts a binary number
  *                  to an unsigned int
@@ -41,38 +38,29 @@ unsigned int _strlen(const char *str)
 */
 unsigned int binary_to_uint(const char *b)
 {
-  int index;
-	unsigned int result = 0, base2 = 1,  num = 0;
-
-	/*if b is NULL return 0*/
-	if (b == NULL)
-		return (0);
-
-	#ifdef DEBUG /*print debug statement*/
-	printf("String is %s, and length is %u.\n", b, _strlen(b));
-	#endif
-
-	/*iterate through string*/
-	for (index = _strlen(b) - 1; index >= 0; index--)
-	{
-		num = _atoi(b[index]); /*convert char to number*/
-
-		#ifdef DEBUG /*print debug statements*/
-		printf("Number is %u, index is %i and base is %u.\n\n", num, index, base2);
-		#endif
-
-		/*if number is not 0 or 1 return 0*/
-		if (num != 0 && num != 1)
-			return (0);
-
-		result += num * base2; /*enable debug to see it in action*/
-		base2 *= 2;
-
-		#ifdef DEBUG /*print debug statements*/
-		printf("Result is %u.\n", result);
-		#endif
-	}
-
-	return (result);
-
+int index;
+unsigned int result = 0, base2 = 1,  num = 0;
+/*if b is NULL return 0*/
+if (b == NULL)
+return (0);
+#ifdef DEBUG /*print debug statement*/
+printf("String is %s, and length is %u.\n", b, _strlen(b));
+#endif
+/*iterate through string*/
+for (index = _strlen(b) - 1; index >= 0; index--)
+{
+num = _atoi(b[index]); /*convert char to number*/
+#ifdef DEBUG /*print debug statements*/
+printf("Number is %u, index is %i and base is %u.\n\n", num, index, base2);
+#endif
+/*if number is not 0 or 1 return 0*/
+if (num != 0 && num != 1)
+return (0);
+result += num * base2; /*enable debug to see it in action*/
+base2 *= 2;
+#ifdef DEBUG /*print debug statements*/
+printf("Result is %u.\n", result);
+#endif
+}
+return (result);
 }
