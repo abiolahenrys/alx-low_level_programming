3bac2d7f567ab30700311c31e9914a5798af369a
Mac <mac@Mars-MacBook-Pro.local>
2022-10-27 12:59:40 +0100

    K

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -12,13 +12,10 @@
 */
 void print_bin(unsigned long int number)
 {
-	if (number >> 1)
-		print_bin(number >> 1);
-
-	putc((number & 1) ? '1' : '0', stdout);
+if (number >> 1)
+print_bin(number >> 1);
+putc((number & 1) ? '1' : '0', stdout);
 }
-
-
 /**
  * get_bit - gets a bit at a given index
  *
@@ -31,22 +28,18 @@ void print_bin(unsigned long int number)
 */
 int get_bit(unsigned long int n, unsigned int index)
 {
-  int bit;
-
-	/*if index is greater than size of n in binary coded decimal*/
-	if (index > INT_BITS)
-		return (-1);
-
-	#ifdef DEBUG
-	printf("\n[%ld >> %d] is %ld.\n", n, index, (n >> index));
-	printf("%ld in binary: ", n);
-	print_bin(n);
-	printf("\n[%ld >> %d] in binary: ", n, index);
-	print_bin(n >> index);
-	printf(" <- last bit is bit at index %d\n\n", index);
-	#endif
-
-	bit = ((n >> index) & 1); /*shift n right by index and with 1 to find bit*/
-
-	return (bit);
+int bit;
+/*if index is greater than size of n in binary coded decimal*/
+if (index > INT_BITS)
+return (-1);
+#ifdef DEBUG
+printf("\n[%ld >> %d] is %ld.\n", n, index, (n >> index));
+printf("%ld in binary: ", n);
+print_bin(n);
+printf("\n[%ld >> %d] in binary: ", n, index);
+print_bin(n >> index);
+printf(" <- last bit is bit at index %d\n\n", index);
+#endif
+bit = ((n >> index) & 1); /*shift n right by index and with 1 to find bit*/
+return (bit);
 }
