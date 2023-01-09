63ac200a33c061dbb804bba59e3df0e05d6c74c3
abiolahenrys <abiolahenrys@gmail.com>
2023-01-09 11:57:38 +0100

    L

--- 0x1A-hash_tables/0-main.c
+++ 0x1A-hash_tables/0-main.c
@@ -1,10 +1,7 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "hash_tables.h"
 
 /**
- * main - check the code for Holberton School students.
+ * main - check the code for
  *
  * Return: Always EXIT_SUCCESS.
  */
